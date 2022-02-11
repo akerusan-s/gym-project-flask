0ff06783267558adc6b5040ec70e48486b190f45
Sergey <ivashchenko_serezha@mail.ru>
2022-02-11 22:47:41 +0300

    remake (Gym sport --> Bmx sport) + models;lending;all lessons;all news;

--- data/__all_models.py
+++ data/__all_models.py
@@ -1,4 +1,5 @@
 import sqlalchemy
+import datetime
 from .db_session import SqlAlchemyBase
 
 
@@ -6,77 +7,101 @@ class Category(SqlAlchemyBase):
     __tablename__ = 'Категории'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    description = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
 
     def __repr__(self):
-        return f'<Category> {self.id} {self.description}'
+        return f'<Category> {self.id} {self.name}'
 
 
-class Muscle(SqlAlchemyBase):
-    __tablename__ = 'Мышцы'
+class ArticleType(SqlAlchemyBase):
+    __tablename__ = 'ВидСтатьи'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    anatomy_desc = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    function_desc = sqlalchemy.Column(sqlalchemy.String, nullable=True)
 
     def __repr__(self):
-        return f'<Muscle> {self.id} {self.name}'
+        return f'<ArticleType> {self.id} {self.name}'
 
 
-class Exercise(SqlAlchemyBase):
-    __tablename__ = 'Упражнения'
-
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    description = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    scheme = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    category = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey(Category.id), nullable=True)
-
-    def __repr__(self):
-        return f'<Exercise> {self.id} {self.name}'
-
-
-class Difficulty(SqlAlchemyBase):
-    __tablename__ = 'Сложности'
-
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    description = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-
-    def __repr__(self):
-        return f'<Difficulty> {self.id} {self.description}'
+# class Muscle(SqlAlchemyBase):
+#     __tablename__ = 'Мышцы'
+#
+#     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+#     name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+#     anatomy_desc = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+#     function_desc = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+#
+#     def __repr__(self):
+#         return f'<Muscle> {self.id} {self.name}'
 
 
-class Program(SqlAlchemyBase):
-    __tablename__ = 'Программы тренировок'
+class User(SqlAlchemyBase):
+    __tablename__ = 'Пользователи'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     description = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    difficulty = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey(Difficulty.id), nullable=True)
 
     def __repr__(self):
-        return f'<Program> {self.id} {self.name}'
+        return f'<User> {self.id} {self.name}'
 
 
-class ExerciseMuscle(SqlAlchemyBase):
-    __tablename__ = 'Упражнения-Мышцы'
+class Article(SqlAlchemyBase):
+    __tablename__ = 'Статья'
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    id_exercise = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey(Exercise.id), nullable=True)
-    id_muscle = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey(Muscle.id), nullable=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    description = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    text = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    category_tag = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey(Category.id), nullable=True)
+    author = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey(User.id), nullable=True)
+    creation_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
+    category_type = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey(ArticleType.id), nullable=True)
 
     def __repr__(self):
-        return f'<ExerciseMuscle> {self.id}: {self.id_exercise} {self.id_muscle}'
+        return f'<Article> {self.id} {self.name} {self.category_type}'
 
 
-class ProgramExercise(SqlAlchemyBase):
-    __tablename__ = 'Программы-Упражнения'
+# class Difficulty(SqlAlchemyBase):
+#     __tablename__ = 'Сложности'
+#
+#     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+#     description = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+#
+#     def __repr__(self):
+#         return f'<Difficulty> {self.id} {self.description}'
 
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    id_exercise = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey(Exercise.id), nullable=True)
-    id_program = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey(Program.id), nullable=True)
-    day_of_training = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
 
-    def __repr__(self):
-        return f'<ProgramExercise> {self.id}: {self.id_exercise} {self.id_program}'
+# class Program(SqlAlchemyBase):
+#     __tablename__ = 'Программы тренировок'
+#
+#     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+#     name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+#     description = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+#     difficulty = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey(Difficulty.id), nullable=True)
+#
+#     def __repr__(self):
+#         return f'<Program> {self.id} {self.name}'
+#
+#
+# class ExerciseMuscle(SqlAlchemyBase):
+#     __tablename__ = 'Упражнения-Мышцы'
+#
+#     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+#     id_exercise = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey(Exercise.id), nullable=True)
+#     id_muscle = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey(Muscle.id), nullable=True)
+#
+#     def __repr__(self):
+#         return f'<ExerciseMuscle> {self.id}: {self.id_exercise} {self.id_muscle}'
+#
+#
+# class ProgramExercise(SqlAlchemyBase):
+#     __tablename__ = 'Программы-Упражнения'
+#
+#     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+#     id_exercise = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey(Exercise.id), nullable=True)
+#     id_program = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey(Program.id), nullable=True)
+#     day_of_training = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+#
+#     def __repr__(self):
+#         return f'<ProgramExercise> {self.id}: {self.id_exercise} {self.id_program}'
--- main.py
+++ main.py
@@ -9,6 +9,11 @@ from data import db_session
 app = Flask(__name__)
 
 
+@app.errorhandler(404)
+def problem_404(e):
+    return render_template("error404.html", title="Страница в разработке")
+
+
 @app.route("/")
 def main_page():
     # db_sess = db_session.create_session()
@@ -16,8 +21,13 @@ def main_page():
     return render_template("lending.html", title="Главная")
 
 
+@app.route("/exercises")
+def exercises():
+    return render_template("exercises.html", title="Обучение")
+
+
 if __name__ == '__main__':
-    db_session.global_init("db/gym-bd.db")
+    db_session.global_init("db/gym-bmx.db")
     db_sess = db_session.create_session()
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
